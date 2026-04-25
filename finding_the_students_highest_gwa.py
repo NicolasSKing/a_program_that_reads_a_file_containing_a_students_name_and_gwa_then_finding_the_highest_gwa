720c9e5582163892b23b8c2139ba362204e18a4d
NicolasSKing <nocoben10@gmail.com>
2026-04-25 16:27:57 +0800

    here_i_made_another_class_this_class_will_be_the_one_to_handle_the_rest_of_the_logic_of_the_program_the_self.students_later_i_will_append_here_all_the_students_with_their_gwa

--- finding_the_students_highest_gwa.py
+++ finding_the_students_highest_gwa.py
@@ -2,4 +2,10 @@ class Students:
     def __init__(self, name, gwa):
         self.name = name
         self.gwa = gwa
+
+class StudentProgram:
+    def __init__(self, filename):
+        self.filename = filename
+        self.students = []
+
         
